64ba2167b2e27c752b0a9432ac9a340cb8859118
Will Lytle <wlytle89@gmail.com>
2020-08-13 21:33:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,8 @@ def monopoly_with_third_tier
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
-        :b_and_o_railroad => {}
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
       }
     } 
   }
