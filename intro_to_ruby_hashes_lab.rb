b32c13f0cf9a12e8fdfdd241b3626f97e7d1b2e8
Will Lytle <wlytle89@gmail.com>
2020-08-13 21:28:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,9 @@ def monopoly_with_third_tier
       :pieces => 4,
       :rent_in_dollars => { 
         :one_piece_owned => 25 
-        :tow_pieces_owned =>50
+        :two_pieces_owned =>50
+        :three_pieces_owned => 75 
+        :four_pieces_owned =>100
       },
       :names => {}
     } 
